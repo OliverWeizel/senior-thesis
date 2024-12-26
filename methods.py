ae06f14f27a29934afac432a524dc46628e2235d
OliverWeizel <oweizel@princeton.edu>
2024-12-25 21:18:32 -0500

    started making very modular methods.py

--- methods.py
+++ methods.py
@@ -1,3 +1,33 @@
-def valid_sentences(textpath, indicator):
-  with open(textpath) as f:
-    
+import re
+
+sentence_enders = [".", '?', '!', ':', ';']
+
+def english_indicator(text, options):
+  for w in text:
+    if w in options:
+      return True
+  return False
+
+def english_processor(text, specific_processor=None):
+  res = []
+  sentences = text
+  for e in sentence_enders:
+    sentences = sentences.split(' '+e+' ')
+  if specific_processor:
+    res = specific_processor(sentences)
+  else:
+    res = [s for s in sentences if all(re.match('^[\w-]+$', w) for w in s)]
+  return res
+  #for s in sentences:
+  #  if all(re.match('^[\w-]+$', w) for w in s):
+  #    res.append(s)
+
+
+def valid_sentences(textpath, processor, indicator):
+  res = []
+  with open(textpath, 'r') as f:
+    for line in f:
+      sentences = [s for s in processor(line) if indicator(s)]
+      res = res + sentences
+  return res
+
