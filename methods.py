7e73add18054cd0a56c1ea87aaf376c40dcb7430
OliverWeizel <oweizel@princeton.edu>
2025-03-30 02:15:38 -0400

    tbd: word vectors, mohammad lexicons, wordnet

--- methods.py
+++ methods.py
@@ -5,18 +5,16 @@ import nltk
 import nltk.draw
 from nltk.tree import Tree
 from nltk.draw.tree import TreeView
+from nltk.stem import WordNetLemmatizer as wln
 import os
 from IPython.display import display
 # import spacy
 from  icecream import ic
 from scipy.stats import chisquare
 
+nltk.download('wordnet')
+
 sentence_enders = [".", '?', '!', ':', ';']
-# nlp = spacy.load('en_core_web_md')
-# if spacy.__version__.startswith('2'):
-#   nlp.add_pipe(benepar.BeneparComponent("benepar_en3"))
-# else:
-#   nlp.add_pipe("benepar", config={"model": "benepar_en3"})
 
 # takes in an array of english words and returns true if one of a 
 # set of words is present
@@ -31,17 +29,19 @@ def english_indicator(text, options=['but']):
 # if containing all alphanumeric characters, or if a specified
 # other condition is met
 def english_processor(text, specific_processor=None):
-  #print("HEllllllLLO")
   res = []
-  #sentences = re.split(' ' + ' | '.join(sentence_enders) + ' ', text)
-  ic(sentences)
-  #sentences = text.split(' . ')
+
+  #https://stackoverflow.com/questions/25735644/python-regex-for-splitting-text-into-sentences-sentence-tokenizing
+  sentences = re.split(r'(?<!\w\.\w.)(?<!\b[A-Z][a-z]\.)(?<![A-Z]\.)(?<=\.|\?)\s|\\n', text)
+
   sentences = [s.split(' ') for s in sentences]
-  ic(sentences)
-  sentences = [[w for w in s if w not in { '\n', '<p>', '', ',' } and w != ''] for s in sentences]
-  #sentences = [s for s in sentences if '@' not in s]
-  ic(sentences)
-  sentences = [s.lower() for s in sentences]
+
+
+  sentences = [[w.lower() for w in s if w not in { '\n', '<p>', '', ',', '.', '\"', '\'', '?', '!', ':', ';'}] for s in sentences]
+
+  sentences = [s for s in sentences if '@' not in s]
+
+  sentences = [s for s in sentences]
 
   for s in sentences:
     if not s:
@@ -57,20 +57,13 @@ def english_processor(text, specific_processor=None):
       continue
     if '-' in joined:
       continue
-
     res.append(s)
-  #print([r[:3] for r in res])
-  # for e in sentence_enders:
-  #   print(e)
-  #   sentences = [s.split(' '+e+' ') for s in sentences]
-  # if specific_processor:
-  #   res = specific_processor(sentences)
-  #else:
-  #  res = [s for s in sentences if all(re.match(r'^[\w-]+$', w) for w in s.split(' '))]
   return res
 
-def new_processor(specific_processor=None):
-  return nltk.tokenize.sent_tokenize(text)
+def new_processor(text, specific_processor=None):
+  res = nltk.sent_tokenize(text)
+  ic(res)
+  return res
 
 
 # takens in the path to a .txt file, a function that takes in a 
@@ -78,7 +71,6 @@ def new_processor(specific_processor=None):
 # of words in sentences, and a function that return true iff a 
 # sentence is valid
 def valid_sentences(textpath, processor, indicator):
-  print("NEW8")
   res = []
   i = 0
   with open(textpath, 'r') as f:
@@ -94,18 +86,59 @@ def pp(t):
 def text(t):
   return ' '.join(t.leaves())
 
+def conjoin(a, b, left):
+  if left:
+    return Tree.fromstring(b.label() + ' (' + text(a) + ' ' + text(b) + ')')
+  else:
+    return Tree.fromstring(a.label() + ' (' + text(a) + ' ' + text(b) + ')')
+  
+def lowest_pos(t):
+  return [s for s in t.subtrees(lambda t: t.height() == 2)]
+
+def lowest_leaves(t):
+  return [t[s] for s in t.subtrees(lambda t: t.height() == 2)]
+
+#returns if corrective, anchored (verb negated)
+def find_s(t, pos0):
+  sentential_tags = {'S', 'SBAR', 'SINV', 'SBARQ', 'SQ'}
+  negators = {'not', 'n\'t'}
+  verb_tags = {'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}
+  tensed = {'have', 'do', 'will', 'would', 'can', 'could', 'may', 'might', 'should', 'ought', 'must', 'shall', 'be'}
+
+  try:
+    while t[pos].label() not in sentential_tags:
+      pos = pos[:-1]
+  except:
+    return (False, False)
+  poses = lowest_pos(t)
+  i = None
+  for k in range(poses.index(pos0)):
+    if text(t[poses[k]]) in negators:
+      i = k
+      break
+  if i == None:
+    return (False, False)
+  try:
+    if t[poses[i-1]].label() in verb_tags and wln().lemmatize(text(t[poses[i-1]]),pos='v') in tensed:
+      return (True, True)
+    return (True, False)
+  except:
+    return (False, False)
+
 def test():
   parser = benepar.Parser("benepar_en3")
-  ans = valid_sentences('COCA/text_academic_rpe/w_acad_1990.txt', english_processor, english_indicator) #[:5]
-  #df = pd.DataFrame({"sentence":ans})
+  ans = valid_sentences('demo.txt', english_processor, english_indicator)
+  ic(ans)
+  df = pd.DataFrame({"sentence":ans})
+  print("OK")
   tree_gen = parser.parse_sents([benepar.InputSentence(s) for s in ans])
   trees = []
   for t in tree_gen:
-    trees.append(t)
+   trees.append(t)
   #ic(trees)
-  #df['tree'] = trees
+  # df['tree'] = trees
 
-  #df = pd.DataFrame("tree":[], "conjunction":[], 'left':[], 'right':[])
+  # df = pd.DataFrame("tree":[], "conjunction":[], 'left':[], 'right':[])
 
   parent_tree = []
   parent_text = []
@@ -160,27 +193,27 @@ def test():
   print(res)
   print(df.head())
 
-  #df.head()
-  # ic(ans[0])
-  # ic(trees[0])
-  # for t in trees:
-  #   with open('trees/'+'test', 'w') as f:
-  #     for tp in t.treepositions():
-  #       if isinstance(t[tp], str):
-  #         continue
-  #       ic(t[tp])
-  #       if t[tp].label() == 'CC':
-  #         parent = t[tp[:-1]]
-  #         f.write(pp(t[tp[:-1]]))
-  #         for stp in parent.treepositions():
-  #           f.write(str(parent[stp]))
-  #           f.write('\n')
-  #         f.write('\n')
-  # #display(trees[0])
-  #trees[0].pretty_print(unicodelines=True, nodedist=4)
-  #TreeView(trees[0])._cframe.print_to_file('output.ps')
-  #os.system('convert output.ps output.png')
-  #ic(df['tree'][0])
+  # #df.head()
+  # # ic(ans[0])
+  # # ic(trees[0])
+  # # for t in trees:
+  # #   with open('trees/'+'test', 'w') as f:
+  # #     for tp in t.treepositions():
+  # #       if isinstance(t[tp], str):
+  # #         continue
+  # #       ic(t[tp])
+  # #       if t[tp].label() == 'CC':
+  # #         parent = t[tp[:-1]]
+  # #         f.write(pp(t[tp[:-1]]))
+  # #         for stp in parent.treepositions():
+  # #           f.write(str(parent[stp]))
+  # #           f.write('\n')
+  # #         f.write('\n')
+  # # #display(trees[0])
+  # #trees[0].pretty_print(unicodelines=True, nodedist=4)
+  # #TreeView(trees[0])._cframe.print_to_file('output.ps')
+  # #os.system('convert output.ps output.png')
+  # #ic(df['tree'][0])
 
 if __name__ == '__main__':
   test()
